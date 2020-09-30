c6da5c4623de875a324177d4e35c875f417b480d
unanonmy <bhchirusaikumar@gmail.com>
2020-10-01 02:41:11 +0530

    gameScript

---
+++ guessinggame.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+function guess {
+echo -n  "Guess,the number of files in this current directory:"
+read n
+
+}
+c=$(ls -1|wc -l)
+while [[ $c -ne $n ]]
+do
+  guess 
+  if [[ $n -gt $c ]]
+  then
+      echo "entered too high"
+  elif [[ $n -lt $c ]]
+  then
+      echo "enterd too low"
+  else
+      echo "......Congrats!! You have guessed correctly...."
+  fi
+done
+path=$PWD
+cd ~
+if [[ -e .bash_profile  ]]
+then
+    echo "export PATH=~$path:$PATH" >> .bash_profile
+    source ~./bash_profile
+fi
